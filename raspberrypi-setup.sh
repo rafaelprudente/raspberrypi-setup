773ec5939ee73e99ed7a665076a081f6d68c0a26
rafaelprudente <rafael.prudente.santos@gmail.com>
2019-12-27 00:04:47 -0300

    20191226

--- raspberrypi-setup.sh
+++ raspberrypi-setup.sh
@@ -12,8 +12,13 @@ if [ ! -d "~/downloads" ]; then
 fi
 
 
-if [ ! -f "~/downloads/setup.sh" ]; then
+if [ ! -f "~/downloads/java/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz" ]; then
 	sudo curl https://download.bell-sw.com/java/11.0.5+11/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz --output ~/downloads/java/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz
+	
+	if [ ! -d "~/downloads/java/jre11.0.5" ]; then
+		mkdir ~/downloads/java/jre11.0.5
+	fi
+
+	tar xvzf ~/downloads/java/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz -C ~/downloads/java/jre11.0.5
 fi
 
-tar xvzf ~/downloads/java/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz -C ~/downloads/java/jre11.0.5
