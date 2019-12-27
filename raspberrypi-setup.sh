85214a17abb0772f3a4d3b530082b2f3545775a2
rafaelprudente <rafael.prudente.santos@gmail.com>
2019-12-27 01:18:11 -0300

    20191226

--- raspberrypi-setup.sh
+++ raspberrypi-setup.sh
@@ -11,21 +11,22 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo -e "${CYAN}---------- ADD REPOSITORY ----------${NC}"
 sudo add-apt-repository ppa:linuxuprising/java
+
 echo 
 echo -e "${CYAN}---------- FIRST UPDATE ----------${NC}"
 sudo apt -yq update
+
 echo 
 echo -e "${CYAN}---------- FIRST UPGRADE ----------${NC}"
 sudo apt -yq full-upgrade
+
 echo 
-echo -e "${CYAN}---------- REMOVE JAVA 8 ----------${NC}"
-sudo apt -yq remove --purge oracle-java8-jdk
+echo -e "${CYAN}---------- REMOVE JAVA ----------${NC}"
+sudo apt -yq remove --purge openjdk*
+
 echo 
-echo -e "${CYAN}---------- REMOVE JAVA 11 ----------${NC}"
-sudo apt -yq remove --purge oracle-java11-jdk
-#echo 
-#echo -e "${CYAN}---------- INSTALL JAVA 8 ----------${NC}"
-#sudo apt -yq install oracle-java8-jdk
-#echo 
-#echo -e "${CYAN}---------- INSTALL JAVA 11 ----------${NC}"
-#sudo apt -yq install oracle-java11-jdk
+echo -e "${CYAN}---------- INSTALL JAVA 8 ----------${NC}"
+sudo apt -yq install openjdk-8-jre
+echo 
+echo -e "${CYAN}---------- INSTALL JAVA 11 ----------${NC}"
+sudo apt -yq install openjdk-11-jre
