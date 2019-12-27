936665aaf60a305dfcc8afac55fcbca35a5c8022
rafaelprudente <rafael.prudente.santos@gmail.com>
2019-12-26 23:54:18 -0300

    20191226

--- raspberrypi-setup.sh
+++ raspberrypi-setup.sh
@@ -7,11 +7,13 @@ cd ~/
 export DEBIAN_FRONTEND=noninteractive
 
 if [ ! -d "~/downloads" ]; then
-	mkdir downloads
+	mkdir ~/downloads
+	mkdir ~/downloads/java
 fi
 
 
 if [ ! -f "~/downloads/setup.sh" ]; then
-	sudo curl https://download.bell-sw.com/java/11.0.5+11/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz --output ~/downloads/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz
+	sudo curl https://download.bell-sw.com/java/11.0.5+11/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz --output ~/downloads/java/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz
 fi
 
+tar xvzf ~/downloads/java/bellsoft-jre11.0.5+11-linux-aarch64.tar.gz -C ~/downloads/java/jre11.0.5
