ca5107d74b1bbee203d6e59dccda8547cdc05f1b
rafaelprudente <rafael.prudente.santos@gmail.com>
2019-12-27 00:50:11 -0300

    20191226

--- raspberrypi-setup.sh
+++ raspberrypi-setup.sh
@@ -9,21 +9,23 @@ cd ~/
 
 export DEBIAN_FRONTEND=noninteractive
 
+echo -e "${CYAN}---------- ADD REPOSITORY ----------${NC}"
+sudo add-apt-repository ppa:linuxuprising/java
+echo 
 echo -e "${CYAN}---------- FIRST UPDATE ----------${NC}"
-echo 
 sudo apt -yq update
+echo 
 echo -e "${CYAN}---------- FIRST UPGRADE ----------${NC}"
-echo 
 sudo apt -yq full-upgrade
+echo 
 echo -e "${CYAN}---------- REMOVE JAVA 8 ----------${NC}"
-echo 
 sudo apt -yq remove --purge oracle-java8-jdk
+echo 
 echo -e "${CYAN}---------- REMOVE JAVA 11 ----------${NC}"
-echo 
 sudo apt -yq remove --purge oracle-java11-jdk
-echo -e "${CYAN}---------- INSTALL JAVA 8 ----------${NC}"
-echo 
-sudo apt -yq install oracle-java8-jdk
-echo -e "${CYAN}---------- INSTALL JAVA 11 ----------${NC}"
-echo 
-sudo apt -yq install oracle-java11-jdk
+#echo 
+#echo -e "${CYAN}---------- INSTALL JAVA 8 ----------${NC}"
+#sudo apt -yq install oracle-java8-jdk
+#echo 
+#echo -e "${CYAN}---------- INSTALL JAVA 11 ----------${NC}"
+#sudo apt -yq install oracle-java11-jdk
