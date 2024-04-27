cd05532885e1ca2737f8c42e86b361bd6b164783
Rafael R Prudente dos Santos <rafael.prudente.santos@gmail.com>
2024-04-27 13:20:06 +0100

    Update script

--- scripts/docker/install.sh
+++ scripts/docker/install.sh
@@ -19,3 +19,27 @@ echo
 echo "${CYAN}---------- Update And Upgrade SO ----------${NC}"
 sudo apt-get update -yq
 sudo apt-get upgrade -yq
+
+echo 
+echo "${CYAN}---------- Add Docker's Official GPG Key ----------${NC}"
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo 
+echo "${CYAN}---------- Add The Repository To Apt Sources ----------${NC}"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+echo 
+echo "${CYAN}---------- Install The Docker Packages ----------${NC}"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo 
+echo "${CYAN}---------- Verify installation ----------${NC}"
+sudo docker run hello-world
